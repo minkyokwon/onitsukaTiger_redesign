91bf481092080aac94947284c471d68862b4ac26
minkyokwon <minkyo4613@gmail.com>
2025-03-07 16:35:28 +0900

    onitsuka_redesign_0307_first

--- js/lib/new_onitsuka_slick.js
+++ js/lib/new_onitsuka_slick.js
@@ -1,3 +1,4 @@
+// NEW ONITSUKA
 $(document).ready(function () {
   $('.slick-track').slick({
     dots: true,
@@ -11,6 +12,46 @@ $(document).ready(function () {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+});
+
+// WHAT IS HOT
+$(document).ready(function () {
+  $('.top_seller .slick-track').slick({
+    dots: true,
+    infinite: true,
+    draggable: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
           slidesToScroll: 1,
           infinite: true,
           dots: true
--- js/onitsukaScript.js
+++ js/onitsukaScript.js
@@ -2,17 +2,42 @@
 console.log("왜 안됨");
 
 // 헤더 이벤트
+// document.addEventListener("DOMContentLoaded", function () {
+//   const header = document.querySelector(".header_wrap");
+
+//   header.addEventListener("mouseover", function () {
+//     header.classList.add("on");
+//   });
+
+//   header.addEventListener("mouseout", function () {
+//     header.classList.remove("on");
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", function () {
   const header = document.querySelector(".header_wrap");
 
   header.addEventListener("mouseover", function () {
-    header.classList.add("on");
+    if (window.scrollY <= 100) {
+      header.classList.add("on");
+    }
   });
 
   header.addEventListener("mouseout", function () {
-    header.classList.remove("on");
+    if (window.scrollY <= 100) {
+      header.classList.remove("on");
+    }
+  });
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 100) {
+      header.classList.add("on");
+    } else {
+      header.classList.remove("on");
+    }
   });
 });
+
 
 // 헤더 이벤트 (스크롤)
 $(function () {
@@ -23,7 +48,7 @@ $(function () {
     var nowScrollTop = $(window).scrollTop();
 
     if (nowScrollTop > prevScrollTop) { $('#header').addClass('down'); }
-    
+
     else { $('#header').removeClass('down'); }
     prevScrollTop = nowScrollTop;
 
@@ -33,11 +58,50 @@ $(function () {
 
 
 // full Page
-$('#fullpage').fullpage({
-  sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-  anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
-  menu: '#menu',
-  slidesNavigation: true
+// $('#fullpage').fullpage({
+//   sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+//   anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+//   menu: '#menu',
+//   slidesNavigation: true
+// });
+
+// Top 버튼 스크롤 이벤트
+$(document).ready(function () {
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('#toTopBtn').fadeIn();
+    } else {
+      $('#toTopBtn').fadeOut();
+    }
+  });
+
+  $('#toTopBtn').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+
+});
+
+// toTopBtn 스크롤 고정
+document.addEventListener("DOMContentLoaded", function () {
+  const toTopBtn = document.getElementById("toTopBtn");
+  const footer = document.querySelector("footer");
+
+  window.addEventListener("scroll", function () {
+    const footerTop = footer.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (footerTop <= windowHeight) {
+      
+      toTopBtn.style.position = "absolute";
+      toTopBtn.style.bottom = `${windowHeight - footerTop + 40}px`;
+    } else {
+      
+      toTopBtn.style.position = "fixed";
+      toTopBtn.style.bottom = "40px";
+    }
+  });
 });
 
 
@@ -56,7 +120,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       });
     },
-    { threshold: 0.2 }
+    { threshold: 0.3 }
   );
 
   observer.observe(footer);
